fb432fa0ab915d9276313b475ccead5adda08ad8
navrajsingh4 <119980499+navrajsingh4@users.noreply.github.com>
2023-09-29 22:20:05 -0600

    week3assignment

---
+++ app/studentinfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function Studentinfo(){
+    return(
+        <main>
+            <h5>Name : Navraj Singh</h5>
+            <h5>Class : CPRG306 F</h5>
+            <h5><Link href="https://github.com/navrajsingh4">GitHub Link</Link></h5>
+            </main>
+            )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import Studentinfo from "../studentinfo";
+
+export default function Homeweek(){
+    return(
+        <main>
+            <b><h1>My Shopping List</h1></b>
+            <Studentinfo/>
+        </main>
+    )
+}
